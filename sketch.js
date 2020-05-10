c78f21307fc3edb5076a45e1c3007acb731264a9
Himansh158 <62837616+Himansh158@users.noreply.github.com>
2020-05-10 18:32:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var rect1, rect2, rect3, rect4, rect5,rect6, t1, t2
+function setup() {
+  createCanvas(800,400);
+  
+}
+
+function draw() {
+  background(0);  
+  rect1 = rect(200,220,50,150);
+  rect2 = rect(250,220,50,150);
+  rect3 = rect(300,220,50,150);
+  rect4 = rect(350,220,50,150);
+  rect5 = rect(275,70,50,150);
+  rect6 = rect(325,70,70,40);
+  t1 = triangle(200,220,250,220,225,185);
+  t2 = triangle(350,220,400,220,375,185 )
+  
+}
